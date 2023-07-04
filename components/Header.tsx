013b1b42d57df866fa76a733191330c2191ce97b
Derek Snyder <dsnyd9494@gmail.com>
2023-07-04 10:04:04 -0700

    liked songs button

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,7 @@ import { RxCaretLeft } from "react-icons/rx";
 import { RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 
 interface HeaderProps{
@@ -104,7 +105,42 @@ return (
             <BiSearch className="text-black" size={20} />
         </button>
     </div>
+        <div className="
+            flex
+            justify-between
+            items-center
+            gap-x-4
+        "
+        >
+            <>
+                <div>
+                    <Button
+                        onClick={() => {}}
+                        className="
+                            bg-transparent
+                            text-neutral-300
+                            font-medium
+                        "
+                    >
+                        Sign Up
+                    </Button>
+                </div>
+                <div>
+                    <Button
+                        onClick={() => {}}
+                        className="
+                            bg-white
+                            px-6
+                            py-2
+                        "
+                    >
+                        Log in
+                    </Button>
+                </div>
+            </>
+        </div>
     </div>
+    {children}
     </div>
   )
 }
